b217f269809151563ab2b837edc407f243d504e7
Adham Elsayed <145601881+AdhamElsayedAI@users.noreply.github.com>
2026-09-25 12:03:17 +0000

    preview: publish V5 UX repair from 069e563ab06f12aa7a98ab3f4a6ef2a2bdf8c9eb

---
+++ v5-ux-preview/_next/static/chunks/3cj7pjdo45_ev.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,28397,t=>{t.v(c=>Promise.all(["static/chunks/1c-19d63lbiu1.js"].map(c=>t.l(c))).then(()=>c(7377)))}]);
